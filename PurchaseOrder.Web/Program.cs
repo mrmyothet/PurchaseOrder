afb8a41086563d3c885ac9fc74ef886cb78dc073
Lin Thit Htoo <lth1212001@gmail.com>
2024-12-14 00:29:17 +0630

    -

--- PurchaseOrder.Web/Program.cs
+++ PurchaseOrder.Web/Program.cs
@@ -2,7 +2,6 @@ using PurchaseOrder.Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddHttpClient(
@@ -17,7 +16,6 @@ builder.Services.AddScoped<HttpClientService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
